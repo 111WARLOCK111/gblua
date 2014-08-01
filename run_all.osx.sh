1aa704ca882931c2b9023a0a98aabeee59325d31
Ali Deym <warlock.killer.gm@gmail.com>
2014-08-01 21:43:48 +0430

    Deleted not needed files + Added glua dll into natives.

--- run_all.osx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-make -f Makefile.OSX
-xbuild KeraLua.Net40.sln /p:Configuration=Release
-cd tests/
-nunit-console KeraLua.Tests.dll
--- run_all.win32.bat
+++
@@ -1,5 +0,0 @@
-call Makefile.Win32.bat
-msbuild KeraLua.Net40.sln /p:Configuration=Release /p:DefineConstants=WSTRING /t:Rebuild
-cd tests/
-nunit-console KeraLua.Tests.dll
-cd ..
